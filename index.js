b342c8d3de98228bb8875b6608b340826df6b449
Angel Soto <85462911+TEODOE-LC101@users.noreply.github.com>
2021-06-17 04:15:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,80 @@
+/*
+let emptyArray = [];
+console.log(emptyArray.length);
+console.log('\n');
+
+let programmingLanguages = ["JavaScript", "Python", "Java", "C#"];
+console.log(programmingLanguages.length);
+console.log('\n');
+
+let classes = ["science, computer, art"];
+console.log(classes.length);
+console.log('\n');
+
+let teachers = ["Jones", "Willoughby", "Rhodes"];
+console.log(teachers.length);
+console.log('\n');
+
+// let programmingLanguages = ["JavaScript", "Python", "Java", "C#"];
+console.log(programmingLanguages[-1]);
+console.log(programmingLanguages[100]);
+console.log('\n');
+
+let javaScriptFrameworks = ["React", "Angular", "Ember"];
+console.log(javaScriptFrameworks);
+
+// Set the value of index 2 to be "Vue"
+javaScriptFrameworks[2] = "Vue";
+
+// Notice the value at index 2 is now "Vue"
+console.log(javaScriptFrameworks);
+console.log('\n');
+
+// METHODS
+console.log(programmingLanguages.indexOf('C#'));
+console.log('\n');
+
+console.log(programmingLanguages.includes('C'));
+console.log('\n');
+
+console.log(programmingLanguages.reverse());
+console.log('\n');
+
+console.log(programmingLanguages.sort());
+console.log('\n');
+
+console.log(programmingLanguages.pop());
+console.log('\n');
+
+console.log(programmingLanguages.push('C','C++','dBase','R','Unity'));
+console.log('\n');
+console.log(programmingLanguages.length);
+console.log(programmingLanguages);
+console.log('\n');
+
+console.log(programmingLanguages.shift());
+console.log('\n');
+
+console.log(programmingLanguages.splice(1));
+console.log('\n');
+
+console.log(programmingLanguages.unshift('TEO', 'DOE'));
+console.log(programmingLanguages);
+console.log('\n');
+
+let writers = ['TEO','HASE','ROLO','SHAN','BUCK','SAM','SPADE'];
+writers.concat(programmingLanguages, writers)
+console.log('\n');
+
+console.log(writers.join('...'));
+console.log('\n');
+
+console.log(writers.slice(0,4));
+console.log('\n');
+
+console.log(writers.split);
+console.log('\n');
+*/
+let charles = ['coder', 'Tech', 47, 23, 350];
+charles.sort();
+console.log(charles);
